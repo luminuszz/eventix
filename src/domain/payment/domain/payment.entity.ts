17986feba8732c11fad623b7294d303c4ef1b434
luminuszz <luminuszz43@gmail.com>
2025-03-09 20:21:43 -0300

    refactor(exception): implement database not found entity exception

--- src/domain/payment/domain/payment.entity.ts
+++ src/domain/payment/domain/payment.entity.ts
@@ -1,9 +1,10 @@
-import {PaymentConfirmedEvent} from '@domain/payment/domain/payment-confirmed.event'
-import {PaymentStatus} from '@domain/payment/domain/payment-status.enum'
-import {DomainEntity} from '@domain/shared/domain.entity'
-import {TicketEntity} from '@domain/ticket/domain/ticket.entity'
-import {UserEntity} from '@domain/users/domain/users.entity'
-import {Column, Entity, ManyToOne} from 'typeorm'
+import { InvalidPaymentOperationError } from '@domain/payment/application/errors/invalid-payment-operation.error'
+import { PaymentConfirmedEvent } from '@domain/payment/domain/payment-confirmed.event'
+import { PaymentStatus } from '@domain/payment/domain/payment-status.enum'
+import { DomainEntity } from '@domain/shared/domain.entity'
+import { TicketEntity } from '@domain/ticket/domain/ticket.entity'
+import { UserEntity } from '@domain/users/domain/users.entity'
+import { Column, Entity, ManyToOne } from 'typeorm'
 
 @Entity('payments')
 export class PaymentEntity extends DomainEntity {
@@ -23,7 +24,12 @@ export class PaymentEntity extends DomainEntity {
   user: UserEntity
 
   markAsPaid() {
+    if (this.status === PaymentStatus.PAID) {
+      throw new InvalidPaymentOperationError('Payment already paid')
+    }
+
     this.status = PaymentStatus.PAID
+
     this.apply(new PaymentConfirmedEvent(this))
   }
 }
