856941ba5482e2aaa83e96402b63a9eb46b6078c
luminuszz <luminuszz43@gmail.com>
2025-03-10 13:12:11 -0300

    feat(ticket): implement ticket fetch

--- src/domain/ticket/application/handlers/on-ticket-created.event.ts
+++ src/domain/ticket/application/handlers/on-ticket-created.event.ts
@@ -1,16 +1,12 @@
-import { TicketCreatedEvent } from '@domain/ticket/domain/events/ticket-created.event'
-import { Logger } from '@nestjs/common'
-import { EventsHandler, IEventHandler } from '@nestjs/cqrs'
+import {TicketCreatedEvent} from '@domain/ticket/domain/events/ticket-created.event'
+import {Logger} from '@nestjs/common'
+import {EventsHandler, IEventHandler} from '@nestjs/cqrs'
 
 @EventsHandler(TicketCreatedEvent)
-export class OnTicketCreatedEventHandler
-  implements IEventHandler<TicketCreatedEvent>
-{
+export class OnTicketCreatedEventHandler implements IEventHandler<TicketCreatedEvent> {
   private logger = new Logger(OnTicketCreatedEventHandler.name)
 
   handle(event: TicketCreatedEvent) {
-    this.logger.log(
-      `[OnTicketCreatedEventHandler] ${event} -> sending email with new ticket`,
-    )
+    this.logger.log(`[OnTicketCreatedEventHandler] ${event} -> sending email with new ticket`)
   }
 }
